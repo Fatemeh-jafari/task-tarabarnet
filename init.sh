0fcbafbd546245a43b31be87b69682536f96bc35
fjkhorasani <67175463+fjkhorasani@users.noreply.github.com>
2024-02-03 00:54:13 +0330

    Add files via upload

---
+++ init.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+docker compose up -d >> stack.log
+
+if [ $? -eq 0 ]; then
+    echo "Docker Compose executed successfully."
+else
+    echo "Error: Docker Compose failed." >&2
+    exit 1
+fi
+
+docker exec -i postgres psql -U postgres -d postgres < populate.sql >> insert-data.log 2>&1
+
+if [ $? -eq 0 ]; then
+    echo "Populate SQL executed successfully."
+else
+    echo "Error: Populate SQL failed." >&2
+    exit 1
+fi
